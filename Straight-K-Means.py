d8743baffaea408177d13191ebc1ec4ea9ebca64
Hulusi Ozyahyalar <hulusierin@gmail.com>
2024-11-19 09:53:07 +0000

    Changed overall to catchup on the past few weeks work

--- Straight-K-Means.py
+++ Straight-K-Means.py
@@ -5,11 +5,10 @@ import random
 def k_means_iteration(data_points, centroids):
     K = len(centroids)
     cluster_assignments = {}
-    S_k = [[] for i in range(K)]
+    S_k = [[] for _ in range(K)]
     
     for i, point in enumerate(data_points):
         distances = []
-        
         for k in range(K):
             dist = np.sum((point - centroids[k])**2)
             distances.append((dist, k))
@@ -29,23 +28,18 @@ def k_means_iteration(data_points, centroids):
 def cluster_recenter(S_k, dimension):
     K = len(S_k)
     new_centroids = []
-
+    
     for k in range(K):
-        if len(S_k[k]) > 0: #Funny check
+        if len(S_k[k]) > 0:
             centroid = []
             for v in range(dimension):
-
-                feature_values = []
-                for point in S_k[k]:
-                    feature_values.append(point[v])
-
+                feature_values = [point[v] for point in S_k[k]]
                 feature_mean = sum(feature_values) / len(feature_values)
                 centroid.append(feature_mean)
-
             new_centroids.append(centroid)
         else:
             new_centroids.append([0] * dimension)
-
+            
     return new_centroids
 
 def square_error(data_points, cluster_assignments, centroids):
@@ -55,12 +49,12 @@ def square_error(data_points, cluster_assignments, centroids):
     
     for k in range(K):
         cluster_error = 0
-        
         cluster_points = []
+        
         for i, assigned_cluster in cluster_assignments.items():
             if assigned_cluster == k:
                 cluster_points.append(data_points[i])
-        
+                
         for point in cluster_points:
             squared_dist = np.sum((point - centroids[k])**2)
             cluster_error += squared_dist
@@ -70,43 +64,58 @@ def square_error(data_points, cluster_assignments, centroids):
         
     return total_error, cluster_errors
 
-df = pd.read_excel('ProcessedData.xlsx')
-Y = df.to_numpy()
-data_points = Y[:, 1:]
-towns = Y[:, 0]
+def run_kmeans(data_points, K, max_iter=1000, n_init=10):
+    best_error = float('inf')
+    best_initial_indices = None
+    best_cluster_assignments = None
+    
+    for init in range(n_init):
+        random_indices = random.sample(range(len(data_points)), K)
+        centroids = [data_points[i] for i in random_indices]
+        
+        last_iteration = None
+        iteration = 0
+        
+        while iteration < max_iter:
+            cluster_assignments, S_k = k_means_iteration(data_points, centroids)
+            
+            if last_iteration and cluster_assignments == last_iteration:
+                break
+                
+            centroids = cluster_recenter(S_k, len(data_points[0]))
+            last_iteration = cluster_assignments
+            iteration += 1
+            
+        total_error, cluster_errors = square_error(data_points, cluster_assignments, centroids)
+        
+        if total_error < best_error:
+            best_error = total_error
+            best_initial_indices = random_indices
+            best_cluster_assignments = cluster_assignments
+            
+    return best_error, best_initial_indices, best_cluster_assignments
+
+input_filename = 'iris.data.data'
+df = pd.read_csv(input_filename)
+data = df.iloc[:, :-1].to_numpy()
+    
 K = int(input("Number of initial clusters: "))
-
-best_error = float('inf')
-best_initial_indices = None
-
-num_initializations = 10
-
-for init in range(num_initializations):
-    random_indices = random.sample(range(len(data_points)), K)
-    centroids = [data_points[i] for i in random_indices]
     
-    last_iteration = None
-    iteration = 0
-    while iteration < 1000:
-        cluster_assignments, S_k = k_means_iteration(data_points, centroids)
-
-        if last_iteration and cluster_assignments == last_iteration:
-            break
-
-        centroids = cluster_recenter(S_k, len(data_points[0]))
-        last_iteration = cluster_assignments
-        iteration += 1
+best_error, best_indices, cluster_assignments = run_kmeans(data, K)
+print("Best error:", best_error)
+print("Best initial centroid indices:", best_indices)
     
-    total_error, cluster_errors = square_error(data_points, cluster_assignments, centroids)
-    if total_error < best_error:
-        best_error = total_error
-        best_initial_indices = random_indices
-        cluster_lists = [[] for i in range(K)]
-        for i, cluster in cluster_assignments.items():
-            cluster_lists[cluster].append(towns[i].strip())
-
-print("Best error: ", best_error)
-print("Best initial centroid indices:",  best_initial_indices)
-print("Best cluster list", cluster_lists)
-print(cluster_assignments)
+cluster_sizes = [0] * K
+for cluster in cluster_assignments.values():
+    cluster_sizes[cluster] += 1
+print("Cluster sizes:", cluster_sizes)
     
+base_filename = input_filename.split('.')[0]
+output_filename = f"{base_filename}.predicted"
+    
+n_samples = len(data)
+final_labels = [cluster_assignments[i] for i in range(n_samples)]
+    
+with open(output_filename, 'w') as f:
+    for label in final_labels:
+        f.write(f"{label}\n")
